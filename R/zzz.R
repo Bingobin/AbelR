4edcdd12f355ddbcdde8932e6e3b3ac79bdbf099
Yabin Liu <liuyabin@sjtu.edu.cn>
2026-07-14 10:58:04 +0800

    feat: enhance package startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,17 @@
 .onAttach <- function(libname, pkgname) {
   version <- utils::packageVersion(pkgname)
-  packageStartupMessage(
-    pkgname,
-    " ",
-    version,
-    " loaded\n",
-    "Reusable bioinformatics analysis and visualization functions."
+  banner <- c(
+    "",
+    "    _    _          _ ____",
+    "   / \\  | |__   ___| |  _ \\",
+    "  / _ \\ | '_ \\ / _ \\ | |_) |",
+    " / ___ \\| |_) |  __/ |  _ <",
+    "/_/   \\_\\_.__/ \\___|_|_| \\_\\",
+    "",
+    paste0("  AbelR ", version, " loaded"),
+    "  Bioinformatics analysis, modelling, and visualization",
+    "  (\u2022\u0300\u1d17\u2022\u0301)\u0648  Ready to explore your data!",
+    ""
   )
+  packageStartupMessage(paste(banner, collapse = "\n"))
 }
